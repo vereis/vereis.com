758f9de0d7557b1ee5975fdd9017c8b6a0484108
vereis <me@vereis.com>
2026-01-27 14:06:25 +0000

    feat(api): add Vereis.version() and Vereis.env() helper functions
    Add centralized functions for retrieving version and environment information:
    - version(): Returns git SHA from RELEASE_SHA env var or git command
    - env(): Returns current Mix environment
    
    These functions are used by GraphQL resolvers to provide version information.

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -23,9 +23,6 @@ config :vereis, Vereis.Repo,
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
 config :vereis, VereisWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
--- api/config/runtime.exs
+++ api/config/runtime.exs
@@ -24,7 +24,9 @@ default_ip = if config_env() == :prod, do: "::", else: "127.0.0.1"
 
 http_ip =
   case System.get_env("HTTP_IP", default_ip) do
-    "::" -> {0, 0, 0, 0, 0, 0, 0, 0}
+    "::" ->
+      {0, 0, 0, 0, 0, 0, 0, 0}
+
     ip_string ->
       ip_string
       |> String.split(".")
@@ -66,13 +68,6 @@ if config_env() == :prod do
 
   config :vereis, VereisWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
-    ],
     secret_key_base: secret_key_base
 
   config :vereis, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
